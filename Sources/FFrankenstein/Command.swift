080ce2939889187ac1912f8d03278fd18d517974
Mark Malstrom <malstrommark@gmail.com>
2019-02-17 18:59:37 -0600

    Refactored Command using SDGExternalProcess

--- Sources/FFrankenstein/Command.swift
+++ Sources/FFrankenstein/Command.swift
@@ -6,146 +6,52 @@
 //
 
 import Foundation
+import SDGExternalProcess
+import Path
 
-public struct Command {
-    public enum Error: Swift.Error, LocalizedError {
-        case unknownError(stderr: String, statusCode: Int)
-        case unknownFileType
-        case noSuchFileOrDirectory
-        
-        public var errorDescription: String? {
-            switch self {
-            case let .unknownError(err, code):
-                return String(localizedKey: "An unknown error with status \(code) occured: \(err)")
-            case .unknownFileType:
-                return String(localizedKey: "The file you are trying to convert is invalid or may not exist.")
-            case .noSuchFileOrDirectory:
-                return String(localizedKey: "The file you are trying to convert does not exist.")
-            }
-        }
+enum Command {
+    private enum Executable {
+        static let which = ExternalProcess(at: URL(fileURLWithPath: "/usr/bin/which"))
+        static let ruby = ExternalProcess(at: URL(fileURLWithPath: "/usr/bin/ruby"))
+        static let curl = ExternalProcess(at: URL(fileURLWithPath: "/usr/bin/curl"))
+        static let brew = ExternalProcess(at: URL(fileURLWithPath: "/usr/local/bin/brew"))
+        static let ffmpeg = ExternalProcess(at: URL(fileURLWithPath: "/usr/local/bin/ffmpeg"))
+        static let ffprobe = ExternalProcess(at: URL(fileURLWithPath: "/usr/local/bin/ffprobe"))
     }
     
-    public enum Executable: String {
-        case ffprobe = "/usr/local/bin/ffprobe"
-        case ffmpeg = "/usr/local/bin/ffmpeg"
-        case trash = "/usr/local/bin/trash"
+    static func ffmpeg(input: Path, arguments: [String], oputput: Path) throws {
+        var args = ["-nostdin", "-i", input.string]
+        args.append(contentsOf: arguments)
+        args.append(oputput.string)
+        try Executable.ffmpeg.run(args)
     }
-
-    @discardableResult
-    public static func run(_ executable: Executable, arguments: [String]) throws -> String {
-        let process = Process()
-        process.launchPath = executable.rawValue
-        process.arguments = arguments
-        return try process.launchWithOutput()
+    
+    static func ffprobe(input: Path) throws -> String {
+        return try Executable.ffprobe.run([
+            "-v", "quiet", "-print_format",
+            "json", "-show_format", "-show_streams",
+            input.string
+        ])
     }
-
-    @discardableResult
-    public static func run(_ executable: Executable, arguments: String) throws -> String {
-        return try Command.run(executable, arguments: [arguments])
-    }
-
-    static func which(_ program: String) throws -> Bool {
-        // True if program exists, false if not
-        let process = Process()
-        process.launchPath = "/usr/bin/which"
-        process.arguments = [program]
-        process.environment = ProcessInfo.processInfo.environment
-        return try !process.launchWithOutput().isEmpty
-    }
-
-    static func brewInstall(_ packageName: String, options: [String] = []) throws {
-        if try !Command.which("brew") {
-            try Command.installHomebrew()
+    
+    static func brewInstall(_ packageName: String, tap: String? = nil, options: [String] = []) throws {
+        if try !Executable.which.run(["brew"]).isEmpty {
+            let homebrewInstallScript = "https://raw.githubusercontent.com/Homebrew/install/master/install"
+            try Executable.ruby.run(["-e", Executable.curl.run(["-fsSL", homebrewInstallScript])])
         }
-
-        print("Installing \(packageName)...")
-
-        let process = Process()
-        process.launchPath = "/usr/local/bin/brew"
-        process.arguments = ["install", packageName] + options
-        try process.launchWithOutput()
+        
+        var package = String()
+        
+        if let tap = tap {
+            try Executable.brew.run(["tap", tap])
+            package.append("\(tap)/")
+        }
+        
+        package.append(packageName)
+        try Executable.brew.run(["install", package] + options)
     }
-
-    private static func installHomebrew() throws {
-        print("Installing homebrew...")
-        let process = Process()
-        process.launchPath = "/usr/bin/ruby"
-        process.arguments = [
-            "-e",
-            "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)".encapsulated(),
-        ]
-        try process.launchWithOutput()
-    }
-}
-
-private extension Process {
-    @discardableResult
-    func launchWithOutput(outputHandle: FileHandle? = nil, errorHandle: FileHandle? = nil) throws -> String {
-        let outputQueue = DispatchQueue(label: "bash-output-queue")
-
-        var outputData = Data()
-        var errorData = Data()
-
-        let outputPipe = Pipe()
-        standardOutput = outputPipe
-
-        let errorPipe = Pipe()
-        standardError = errorPipe
-
-        outputPipe.fileHandleForReading.readabilityHandler = { handler in
-            outputQueue.async {
-                let data = handler.availableData
-                outputData.append(data)
-                outputHandle?.write(data)
-            }
-        }
-
-        errorPipe.fileHandleForReading.readabilityHandler = { handler in
-            outputQueue.async {
-                let data = handler.availableData
-                errorData.append(data)
-                errorHandle?.write(data)
-            }
-        }
-
-        launch()
-        waitUntilExit()
-
-        outputHandle?.closeFile()
-        errorHandle?.closeFile()
-
-        outputPipe.fileHandleForReading.readabilityHandler = nil
-        errorPipe.fileHandleForReading.readabilityHandler = nil
-
-        switch terminationStatus {
-        case 0: return outputData.shellOutput()
-        case 1: return ""
-        default:
-            let errorMessage = errorData.shellOutput()
-
-            if errorMessage.contains("No such file or directory") {
-                throw Command.Error.noSuchFileOrDirectory
-            } else {
-                throw Command.Error.unknownError(
-                    stderr: errorMessage,
-                    statusCode: Int(terminationStatus)
-                )
-            }
-        }
-    }
-}
-
-private extension Data {
-    func shellOutput() -> String {
-        guard let output = String(data: self, encoding: .utf8) else {
-            return ""
-        }
-
-        guard !output.hasSuffix("\n") else {
-            let endIndex = output.index(before: output.endIndex)
-            return String(output[..<endIndex])
-        }
-
-        return output
-    }
+    
+//    private static func test() throws {
+//        try brewInstall("ffmpeg", tap: "slhck/ffmpeg", options: ["--with-aom", "--with-fdk-aac"])
+//    }
 }
