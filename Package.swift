751bd747d369efbab237b09db1452627a822b1e0
Mark Malström <mark@malstrom.me>
2019-03-26 00:57:33 -0500

    Updated with Swift 5 support
    And fixed minimum deployment target in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,14 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FFFrankenstein",
-    products: [
-        .library(name: "FFFrankenstein", targets: ["FFFrankenstein"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.2.0")
-    ],
-    targets: [
-        .target(name: "FFFrankenstein", dependencies: ["ShellOut"]),
-    ]
+    platforms: [.macOS(.v10_12)],
+//    providers: [.brew(["varenc/ffmpeg/ffmpeg"])],
+    products: [.library(name: "FFFrankenstein", targets: ["FFFrankenstein"])],
+    dependencies: [.package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.2.0")],
+    targets: [.target(name: "FFFrankenstein", dependencies: ["ShellOut"])],
+    swiftLanguageVersions: [.v5]
 )
